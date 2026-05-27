269bf5dc88bf73e3eeaec733867b9be488ce2abb
luminecosmeticos5-ctrl <luminecosmeticos5@gmail.com>
2026-05-27 13:26:10 -0300

    Update HomeComponents.tsx

--- src/components/home/HomeComponents.tsx
+++ src/components/home/HomeComponents.tsx
@@ -91,7 +91,7 @@ export const HeroBanner = () => {
                     fallbackBanner.imagem_desktop
                   }
                   alt={banner.titulo || banner.title || 'Banner'}
-                  className="w-full h-full object-cover object-center"
+                  className="w-full h-full object-contain md:object-cover object-center bg-black"
                   loading={index === 0 ? 'eager' : 'lazy'}
                   onError={(e: any) => {
                     e.target.src = fallbackBanner.imagem_desktop;
